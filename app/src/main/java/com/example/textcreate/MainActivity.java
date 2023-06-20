361c7b7bc0932908b2224dfd03c82edd2152e69b
arunhegde <arunhegde1231@outlook.com>
2023-06-20 23:50:46 +0530

    Seventh Commit

--- app/src/main/java/com/example/textcreate/MainActivity.java
+++ app/src/main/java/com/example/textcreate/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.textcreate;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -14,8 +22,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int READ_REQUEST_CODE =42 ;
     private EditText editText;
     private EditText filename;
 
@@ -91,26 +102,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openFile() {
-        if (file == null || !file.exists()) {
-            Toast.makeText(MainActivity.this, "File does not exist", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*"); // Set the MIME type to all files
 
-        try {
-            StringBuilder stringBuilder = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
-                stringBuilder.append('\n');
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                openSelectedFile(uri);
             }
-            reader.close();
+        }
+    }
 
-            editText.setText(stringBuilder.toString());
-            Toast.makeText(MainActivity.this, "File opened successfully", Toast.LENGTH_SHORT).show();
+    private void openSelectedFile(Uri uri) {
+        try {
+            String fileName = getFileName(uri);
+            filename.setText(fileName);
+
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            if (inputStream != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append('\n');
+                }
+                reader.close();
+
+                editText.setText(stringBuilder.toString());
+                Toast.makeText(MainActivity.this, "File opened successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Failed to open file", Toast.LENGTH_SHORT).show();
+            }
         } catch (IOException e) {
             Toast.makeText(MainActivity.this, "Failed to open file", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
+    }
+
+
+    @SuppressLint("Range")
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result;
     }
 }
